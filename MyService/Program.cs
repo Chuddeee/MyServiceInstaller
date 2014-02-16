a570ca01472c2b0e11ce145a231828942d39ee32
Viacheslav Ivanov <viacheslav.ivanov@gmail.com>
2014-02-16 18:46:05 +0400

    Initial commin

---
+++ MyService/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace MyService
+{
+  internal static class Program
+  {
+    private const int SuccessExitCode = 0;
+    private const int FailedExitCode = 1;
+
+    private static int Main(string[] args) {
+      try {
+        if(ServiceInstall.Install<ProjectInstaller>(args)) {
+          return SuccessExitCode;
+        } else if(CommandLine.Contains(args, CommandLine.Service)) {
+          return StartService();
+        } else {
+          return StartConsole();
+        }//if
+      } catch(Exception ex) {
+        Console.WriteLine(ex);
+        return FailedExitCode;
+      }//try
+    }
+
+    private static int StartService() {
+      using(var service = new MyService()) {
+        //Debugger.Break();
+        ServiceBase.Run(service);
+        return service.ExitCode;
+      }//using
+    }
+
+    private static int StartConsole() {
+      Console.Write("Service started. Press <Enter> for stop service and exit.");
+      Console.ReadLine();
+      return SuccessExitCode;
+    }
+  }
+}
